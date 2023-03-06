256b5dede0a8eab7fcf52832edf35531415dfc3a
technophyle66 <adityasharma181103@gmail.com>
2023-03-06 15:58:33 +0530

    deploy

---
+++ src/data.js
@@ -0,0 +1,28 @@
+
+export const projects = [
+    {
+      title: "Chat App",
+      subtitle: "Real-Time Chat App",
+      description:
+        "A real-time chatting app built using React App and Socket.io with HarperDB functionality to store messages",
+      image: "./noun-image-5519951.svg",
+      link: "https://github.com/technophyle66/real-time-chat",
+    },
+    {
+      title: "Coming Soon",
+      subtitle: "Project 2",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./noun-image-5519951.svg",
+      link: "https://github.com/technophyle66",
+    },
+  ];
+
+  export const skills = [
+    "JavaScript",
+    "React",
+    "Next.js",
+    "Node",
+    "Python",
+    "Git"
+  ];
